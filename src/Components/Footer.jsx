10029cbd623e29175a96bbb206dcf1d0bd963ce2
Punit <Punitmistr@gmail.com>
2023-08-04 09:19:00 +0530

    Updated the UI

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
-    <div className='w-full h-40 bg-blue-800 text-center  text-white'>
-       <div className='w-full bg-gray-700 hover:bg-gray-600 font-bold font-sans'>
-       <button className='w-full h-12 '>
-        Back to Top 
-       </button>
-       </div>
-       <br/>
-        <div>
-            test this is the foooter 
-        </div>
-    </div>  
-  )
-}
+    <div className="w-full h-40 bg-blue-800 text-white text-center">
+      <div className="w-full bg-gray-700 hover:bg-gray-600 font-bold font-sans">
+        <button
+          className="w-full h-12 "
+          onClick={scrollToTop}
+        >
+          Back to Top
+        </button>
+      </div>
+      <br />
+      <div>
+        <p>Contact Us: support@example.com | Phone: +1-123-456-7890</p>
+        <p>
+          &copy; {new Date().getFullYear()} Your Ecommerce Store. All rights
+          reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
